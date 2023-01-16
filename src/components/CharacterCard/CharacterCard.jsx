423fb168d7458f91439b0873c7f4cfb55f1ed7c0
Gonzalo <tomas_salda@hotmail.com>
2023-01-16 16:27:13 -0300

    Implementacion de estilo a las cartas

--- src/components/CharacterCard/CharacterCard.jsx
+++ src/components/CharacterCard/CharacterCard.jsx
@@ -2,14 +2,21 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import './index.css'
 
-const CharacterCard = ({item}) => {
+const CharacterCard = ({ item }) => {
   return (
     <>
-    
-        <Link className='card flex flex-col' to={`character/${item.id}`}>
-            <img className=' object-cover w-[150px] h-[150px] rounded-[150px] self-center mt-3' src={`${item.thumbnail.path}.${item.thumbnail.extension}`} alt="Character" />
-            <p className='text-center font-semibold text-white text-2xl'>{item.name}</p>
-        </Link>
+
+      <Link className='card' to={`character/${item.id}`}>
+        <div className='card-front flex flex-col items-center'>
+          <img className=' object-cover w-[150px] h-[150px] rounded-[150px] mt-3' src={`${item.thumbnail.path}.${item.thumbnail.extension}`} alt="Character" />
+          <p className='text-center font-semibold text-white text-2xl'>{item.name}</p>
+        </div>
+        <div className='card-back flex flex-col items-center'>
+          <img className=' object-cover w-[150px] h-[150px] rounded-[150px] mt-3' src={`${item.thumbnail.path}.${item.thumbnail.extension}`} alt="Character" />
+          <p className='text-center font-semibold text-white text-2xl'>{item.name}</p>
+        </div>
+
+      </Link>
 
     </>
   )
