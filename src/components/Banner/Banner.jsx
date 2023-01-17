543712a288260e3b7756c0f26f7f046626ac4224
Gonzalo <tomas_salda@hotmail.com>
2023-01-17 15:11:27 -0300

    Cambio de lg a md Loki

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -13,7 +13,7 @@ const Banner = () => {
           <p className='max-w-[450px] text-base font-semibold mx-6 '>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odit exercitationem fugit omnis est dolore sint ex, voluptatibus a eos quos! Delectus, repellendus.</p>
         </div>
 
-        <div className='flex-1 hidden lg:block'>
+        <div className='flex-1 hidden md:block'>
           <div className="container-img"></div>
           <Link to={`/character/1009407`}>
             <img className='loki' src="https://css-layout-seven.vercel.app/assets/img/avengers.png" alt="" />
